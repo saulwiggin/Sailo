439f3ba0f61dd3ebd9ba7581c055e0aefa9f1be5
Saul Joseph Wiggin <saulwiggin@googlemail.com>
2014-09-29 09:47:33 +0100

    add bootstrap to RoR status list

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,5 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  config.assets.initialize_on_precompile = false
 end
